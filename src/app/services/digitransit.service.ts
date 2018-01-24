55bf5bdae7069a864c43dc6a624b20a4a123a5c5
goebers <kack@cock.li>
2018-01-24 15:41:39 +0200

    lol hups

--- src/app/services/digitransit.service.ts
+++ src/app/services/digitransit.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { HttpHeaders } from '@angular/common/http/';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class DigitransitService {
